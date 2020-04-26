bb487ca1661f4ad6a6512b4f7cd8b5ea1a25decd
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-04-26 18:30:28 +0800

    AST

--- Runtime/Private/MatC/MDL/MDLAST.h
+++ Runtime/Private/MatC/MDL/MDLAST.h
@@ -1,13 +1,19 @@
-#ifndef _MDLAST_H_
-#define _MDLAST_H_ 1
+#ifndef _PT_MATC_MDL_MDLASL_H_
+#define _PT_MATC_MDL_MDLASL_H_ 1
 
-struct MDL_Declaration
+struct PT_MatC_MDL_Declaration
 {
+
 };
 
-struct MDL_Declaration_Module : public MDL_Declaration
+struct PT_MatC_MDL_Declaration_import : public PT_MatC_MDL_Declaration
 {
-    
+
+};
+
+struct PT_MatC_MDL_Declaration_module : public PT_MatC_MDL_Declaration
+{
+
 };
 
 #endif
