129ce67d491431d308fd4c227be515ec5745c731
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-04-27 02:35:30 +0800

    refine YACC

--- Runtime/Private/MatC/MDL/MDLFrontend.cpp
+++ Runtime/Private/MatC/MDL/MDLFrontend.cpp
@@ -5,6 +5,7 @@
 #include <memory.h>
 #include <iostream>
 #include <new>
+#include <assert.h>
 
 extern "C" int mdl_lllex_init_extra(void *user_defined, struct llscan_t **scanner);
 extern "C" void mdl_llset_in(void *user_inputstream, struct llscan_t *scanner);
@@ -33,12 +34,20 @@ std::string *MDLFrontend::Callback_CreateString(char const *s)
 
 std::string *MDLFrontend::Callback_StringAppend(std::string *l, std::string *r)
 {
-    return &((*l).append((*r)));
+    std::string &ret = ((*l).append((*r)));
+#ifndef NDEBUG
+    (*r).clear();
+#endif
+    return &ret;
 }
 
 std::string *MDLFrontend::Callback_StringAppend(std::string *l, char const *m, std::string *r)
 {
-    return &(((*l).append(m)).append((*r)));
+    std::string &ret = (((*l).append(m)).append(*r));
+#ifndef NDEBUG
+    (*r).clear();
+#endif
+    return &ret;
 }
 
 template <typename T>
@@ -49,6 +58,9 @@ void _Invoke_Destructor_Helper(T *_self)
 
 void MDLFrontend::Callback_DisposeString(std::string *s)
 {
+#ifndef NDEBUG
+    assert((*s).empty());
+#endif
     _Invoke_Destructor_Helper(s);
     Callback_Free(s);
 }
@@ -57,6 +69,9 @@ void MDLFrontend::Callback_HashTypeName(std::string *s)
 {
     char const *ident = (*s).c_str();
     ident = NULL;
+#ifndef NDEBUG
+    (*s).clear();
+#endif
 }
 
 void MDLFrontend::Callback_Error(char const *s)
