41fa70e089d35731baa24b66a6f62e56b32b84bb
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-04-29 00:51:35 +0800

    refine

--- Runtime/Solution/PosixLinuxX11/PTMatC-Test.mk
+++ Runtime/Solution/PosixLinuxX11/PTMatC-Test.mk
@@ -17,7 +17,6 @@ LOCAL_CPPFLAGS += -std=c++11
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../ThirdParty/PosixLinuxX11/Bionic-Redistributable/include/
 
-LOCAL_LDFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
 LOCAL_LDFLAGS += -Wl,--enable-new-dtags # the linker can't recognize the old dtags
 LOCAL_LDFLAGS += -Wl,-rpath,/XXXXXX # chrpath can only make path shorter
 
--- Runtime/Solution/PosixLinuxX11/libPTMatC.mk
+++ Runtime/Solution/PosixLinuxX11/libPTMatC.mk
@@ -17,7 +17,6 @@ LOCAL_CFLAGS += -DPTMATCAPI=PTEXPORT
 	
 LOCAL_CPPFLAGS += -std=c++11
 
-LOCAL_LDFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
 LOCAL_LDFLAGS += -Wl,--enable-new-dtags # the linker can't recognize the old dtags
 LOCAL_LDFLAGS += -Wl,-rpath,/XXXXXX # chrpath can only make path shorter
 
@@ -42,8 +41,6 @@ LOCAL_CFLAGS += -DPTMATCAPI=PTEXPORT
 
 LOCAL_CPPFLAGS += -std=c++11
 
-LOCAL_LDFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
-
 LOCAL_CPP_FEATURES := exceptions
 
 include $(BUILD_STATIC_LIBRARY)
@@ -63,9 +60,7 @@ LOCAL_CFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
 LOCAL_CFLAGS += -fvisibility=hidden
 LOCAL_CFLAGS += -DPTMATCAPI=PTEXPORT
 
-LOCAL_CFLAGS += -x c #to use C compiler
+LOCAL_CFLAGS += -x c # to use the C compiler
 LOCAL_CFLAGS += -std=c99
-
-LOCAL_LDFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
 
 include $(BUILD_STATIC_LIBRARY)
