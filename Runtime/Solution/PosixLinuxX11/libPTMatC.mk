437ac04301e38918c0465b49983fd9ff19002a13
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-04-28 14:57:16 +0800

    refine Linux projects

--- Runtime/Solution/PosixLinuxX11/libPTMatC.mk
+++ Runtime/Solution/PosixLinuxX11/libPTMatC.mk
@@ -21,42 +21,51 @@ LOCAL_LDFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
 LOCAL_LDFLAGS += -Wl,--enable-new-dtags # the linker can't recognize the old dtags
 LOCAL_LDFLAGS += -Wl,-rpath,/XXXXXX # chrpath can only make path shorter
 
-LOCAL_CPP_FEATURES := exceptions
-
-LOCAL_STATIC_LIBRARIES := libMDLScannerLex libMDLParserYacc
+LOCAL_STATIC_LIBRARIES := libPTMatC-CXX-Exceptions libPTMatC-C
 
 include $(BUILD_SHARED_LIBRARY)
 
-# libMDLScannerLex
+# libPTMatC-CXX-Exceptions
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libMDLScannerLex
+LOCAL_MODULE := libPTMatC-CXX-Exceptions
 
 LOCAL_SRC_FILES:= \
-	$(LOCAL_PATH)/../../Private/MatC/MDL/MDLScannerLex.c \
+	$(LOCAL_PATH)/../../Private/MatC/MDL/MDLScannerCXX.cpp \
+	$(LOCAL_PATH)/../../Private/MatC/MDL/MDLParserCXX.cpp \
 
-LOCAL_CFLAGS += -x c #to use C compiler
 LOCAL_CFLAGS += -fdiagnostics-format=msvc
 LOCAL_CFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
 LOCAL_CFLAGS += -fvisibility=hidden
 LOCAL_CFLAGS += -DPTMATCAPI=PTEXPORT
+
+LOCAL_CPPFLAGS += -std=c++11
+
+LOCAL_LDFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
+
+LOCAL_CPP_FEATURES := exceptions
 
 include $(BUILD_STATIC_LIBRARY)
 
-# libMDLParserYacc
+# libPTMatC-C
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libMDLParserYacc
+LOCAL_MODULE := libPTMatC-C
 
 LOCAL_SRC_FILES:= \
+	$(LOCAL_PATH)/../../Private/MatC/MDL/MDLScannerLex.c \
 	$(LOCAL_PATH)/../../Private/MatC/MDL/MDLParserYacc.c \
 
-LOCAL_CFLAGS += -x c
 LOCAL_CFLAGS += -fdiagnostics-format=msvc
 LOCAL_CFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
 LOCAL_CFLAGS += -fvisibility=hidden
 LOCAL_CFLAGS += -DPTMATCAPI=PTEXPORT
+
+LOCAL_CFLAGS += -x c #to use C compiler
+LOCAL_CFLAGS += -std=c99
+
+LOCAL_LDFLAGS += -finput-charset=UTF-8 -fexec-charset=UTF-8
 
 include $(BUILD_STATIC_LIBRARY)
